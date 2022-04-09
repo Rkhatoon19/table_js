58c2dbd91fe84f116c059afa4dca431df89d3eeb
Rubi <rubina.khatoon.7587@gmail.com>
2022-04-09 14:25:25 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function table_created(){
+    let n=document.getElementById("inputPassword6").value;
+
+    for(let i=1;i<=10;i++)
+    {
+        document.write(n + " * "+i+" = "+ n*i + "<br>");
+        
+    }
+}
